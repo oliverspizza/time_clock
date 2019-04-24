4ca06c19d14617ddf90ca1f24c81d8a99443d3e3
oliverspizza <34325859+oliverspizza@users.noreply.github.com>
2019-04-24 19:04:28 -0400

    Add files via upload

--- schapp/timeClock/models.py
+++ schapp/timeClock/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from people.models import Employee
+from people.models import CustomUser
 
 class Clock_In(models.Model):
     #creates time employee clocked in.
     punch_in = models.TimeField('Clock in: ',auto_now_add=True)
-    employee = models.ForeignKey(Employee,on_delete=models.CASCADE)
+    employee = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
 
 
 class Clock_Out(models.Model):
     # creates time employee clocked out.
     punch_out = models.TimeField('Clock out: ',auto_now_add=True)
-    employee = models.ForeignKey(Employee,on_delete=models.CASCADE)
+    employee = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
